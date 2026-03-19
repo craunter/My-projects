31bc60dc9b452be50f5a7ad21b2fde6930b0a2a3
brandsaverin <brandsaver.in@gmail.com>
2026-03-19 20:44:27 +0530

    js fix try

--- api/folders.js
+++ api/folders.js
@@ -2,17 +2,17 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = (req, res) => {
-  const basePath = process.cwd();
+  const basePath = path.join(__dirname, '..'); // 🔥 FIXED PATH
 
   const items = fs.readdirSync(basePath, { withFileTypes: true });
 
   const folders = items
     .filter(item =>
       item.isDirectory() &&
-      !item.name.startsWith('.') &&   // ❌ hides .git, .vercel, etc. \
-      !item.name.startsWith('_') &&   // hides ___v
+      !item.name.startsWith('.') &&
+      !item.name.startsWith('_') &&
       item.name !== 'api' &&
-      item.name !== 'node_modules'  
+      item.name !== 'node_modules'
     )
     .map(item => item.name);
 
