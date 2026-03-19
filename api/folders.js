00b0713e11f1c24c38c0ab8cf50fb40ce97e8fc5
brandsaverin <brandsaver.in@gmail.com>
2026-03-19 20:37:47 +0530

    Index fix

--- api/folders.js
+++ api/folders.js
@@ -9,9 +9,10 @@ module.exports = (req, res) => {
   const folders = items
     .filter(item =>
       item.isDirectory() &&
-      !item.name.startsWith('.') &&   // ❌ hides .git, .vercel, etc.
+      !item.name.startsWith('.') &&   // ❌ hides .git, .vercel, etc. \
+      !item.name.startsWith('_') &&   // hides ___v
       item.name !== 'api' &&
-      item.name !== 'node_modules'
+      item.name !== 'node_modules'  
     )
     .map(item => item.name);
 
