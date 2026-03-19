dfa88b7a36f4513f22ba22aac7879621bfa52af9
brandsaverin <brandsaver.in@gmail.com>
2026-03-19 22:33:57 +0530

    fixed api conflict

--- api/folders.js
+++ api/folders.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = (req, res) => {
-  const filePath = path.join(process.cwd(), 'api', 'folders.json'); // ✅ FIXED PATH
+  const filePath = path.join(process.cwd(), 'api', 'data.json'); // ✅ FIXED PATH
   const folders = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
   res.status(200).json(folders);
 };
